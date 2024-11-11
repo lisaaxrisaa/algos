1d25f918e7c680e3593efc5386f18a22ca8d3390
Lisa Fujita <lisafujita90@gmail.com>
2024-11-11 10:16:54 -0500

    JS coding practice

--- arrays.js
+++ arrays.js
@@ -25,6 +25,68 @@ function sumDouble(a, b) {
   return a + b;
 }
 
+// Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+function diff21(n) {
+  if (n < 21) {
+    return 21 - n;
+  } else if (n > 21) {
+    return (n - 21) * 2;
+  }
+  return 0;
+}
+
+// We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+function parrotTrouble(talking, hour) {
+  if ((talking && hour < 7) || (talking && hour > 20)) {
+    return true;
+  }
+  return false;
+}
+
+// Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+function makes10(a, b) {
+  if (a === 10 || b === 10 || a + b === 10) {
+    return true;
+  }
+  return false;
+}
+
+// Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+function nearHundred(n) {
+  if ((n >= 90 && n <= 110) || (n >= 190 && n <= 210)) {
+    return true;
+  }
+  return false;
+}
+
+// Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+function posNeg(a, b, negative) {
+  if (negative) {
+    return a < 0 && b < 0;
+  } else if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+    return true;
+  }
+  return false;
+}
+
+// Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+// Examples
+// notString('candy') → not candy
+// notString('x') → not x
+// notString('not bad') → not bad
+
+function notString(str) {
+  if (str.slice(0, 3) !== 'not') {
+    return 'not ' + str;
+  }
+  return str;
+}
+
 // Given 2 strings, a and b, return the number of the positions where they contain
 // the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx"
 // "xx", "aa", and "az" substrings appear in the same place in both strings.
