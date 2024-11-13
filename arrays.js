a07aa0900470ca37523aff24e1f59635568837a4
Lisa Fujita <lisafujita90@gmail.com>
2024-11-13 10:50:54 -0500

    JS practice problems

--- arrays.js
+++ arrays.js
@@ -87,6 +87,233 @@ function notString(str) {
   return str;
 }
 
+// Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+// Examples
+// missingChar('kitten', 1) → ktten
+// missingChar('kitten', 0) → itten
+// missingChar('kitten', 4) → kittn
+
+function missingChar(str, n) {
+  return str.slice(0, n) + str.slice(n + 1);
+}
+
+// Given a string, return a new string where the first and last chars have been exchanged.
+// Examples
+// frontBack('code') → eodc
+// frontBack('a') → a
+// frontBack('ab') → ba
+
+function frontBack(str) {
+  if (str.length <= 1) {
+    return str;
+  }
+  return str.slice(-1) + str.slice(1, str.length - 1) + str.slice(0, 1);
+}
+
+// Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+// Examples
+// front3('Java') → JavJavJav
+// front3('Chocolate') → ChoChoCho
+// front3('abc') → abcabcabc
+
+function front3(str) {
+  return str.slice(0, 3).repeat(3);
+}
+
+// Given a string, take the last char and return a new string with the last char added at the front and back, so 'cat' yields 'tcatt' The original string will be length 1 or more.
+// Examples
+// backAround('cat') → tcatt
+// backAround('Hello') → oHelloo
+// backAround('a') → aaa
+
+function backAround(str) {
+  return str.slice(-1) + str + str.slice(-1);
+}
+
+// Return true if the given non-negative number is a multiple of 3 or a multiple of 5. (Hint: Use the % 'mod' operator)
+// Examples
+// or35(3) → true
+// or35(10) → true
+// or35(8) → false
+
+function or35(n) {
+  if (n % 3 === 0 || n % 5 === 0) {
+    return true;
+  }
+  return false;
+}
+
+// Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so 'kitten' yields 'kikittenki' chars are there.
+// Examples
+// front22('kitten') → kikittenki
+// front22('Ha') → HaHaHa
+// front22('abc') → ababcab
+
+function front22(str) {
+  if (str.length <= 1) {
+    return str.repeat(3);
+  }
+  return str.slice(0, 2) + str + str.slice(0, 2);
+}
+
+// Given a string, return true if the string starts with 'hi'and false otherwise.
+// Examples
+// startHi('hi there') → true
+// startHi('hi') → true
+// startHi('hello hi') → false
+
+function startHi(str) {
+  if (str.slice(0, 2) === 'hi') {
+    return true;
+  }
+  return false;
+}
+
+// Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+// Examples
+// icyHot(120, -1) → true
+// icyHot(-1, 120) → true
+// icyHot(2, 120) → false
+
+function icyHot(temp1, temp2){
+  if((temp1 < 0 && temp2 > 100) || 
+     (temp2 < 0 && temp1 > 100) {
+     return true;
+     }
+  return false;
+}
+
+// Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+// Examples
+// in1020(12, 99) → true
+// in1020(21, 12) → true
+// in1020(8, 99) → false
+
+function in1020(a, b){
+  if((a >= 10 && a <= 20)||
+     (b >= 10 && b <= 20)) {
+  return true;
+  }
+  return false;
+}
+
+// We'll say that a number is 'teen' if it is in the range 13..19 inclusive. Given 3 int values, return true if 1 or more of them are teen.
+// Examples
+// hasTeen(13, 20, 10) → true
+// hasTeen(20, 19, 10) → true
+// hasTeen(20, 10, 13) → true
+
+function hasTeen(a, b, c){
+  if((a >= 13 && a <= 19)||
+    (b >= 13 && b <= 19)||
+    (c >= 13 && c <= 19)) {
+  return true;
+  }
+  return false;
+}
+
+// We'll say that a number is 'teen' if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+// Examples
+// loneTeen(13, 99) → true
+// loneTeen(21, 19) → true
+// loneTeen(13, 13) → false
+
+function loneTeen(a, b){
+  const teenA = (a >= 13 && a <= 19)
+  const teenB = (b >= 13 && b <= 19)
+  if( !teenA && teenB || teenA && !teenB) {
+  return true;
+  }
+    return false;
+  }
+
+//   Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+// Examples
+// delDel('adelbc') → abc
+// delDel('adelHello') → aHello
+// delDel('abcdel') → abcdel
+
+function delDel(str){
+  if(str.slice(1,4) === 'del') {
+  return str.slice(0,1) + str.slice(4)
+  }
+  return str;
+}
+
+// Return true if the given string begins with 'mix', except the 'm' can be anything, so 'pix', '9ix' .. all count.
+// Examples
+// mixStart('mix snacks') → true
+// mixStart('pix snacks') → true
+// mixStart('piz snacks') → false
+
+function mixStart(str){
+  if(str.match(/.ix/g)) {
+  return true;
+  }
+  return false;
+}
+
+// Given three int values, a b c, return the largest.
+// Examples
+// intMax(1, 2, 3) → 3
+// intMax(1, 3, 2) → 3
+// intMax(3, 2, 1) → 3
+
+function intMax(a, b, c){
+  return Math.max(a, b, c)
+  }
+
+//   Given 2 int values, return whichever value is nearest to the value 10, or return 0 in the event of a tie. Note that Math.abs(n) returns the absolute value of a number.
+// Examples
+// close10(8, 13) → 8
+// close10(13, 8) → 8
+// close10(13, 7) → 0
+
+function close10(a, b){
+  const numA = Math.abs(a - 10);
+  const numB = Math.abs(b - 10);
+  if(numA < numB) {
+  return a;
+  }else if (numB < numA){
+  return b;
+  }
+  return 0
+}
+
+// Given 2 int values, return true if they are both in the range 30..40 inclusive, or they are both in the range 40..50 inclusive.
+// Examples
+// in3050(30, 31) → true
+// in3050(30, 41) → false
+// in3050(40, 50) → true
+
+function in3050(a, b){
+  if((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) {
+  return true;
+  }else if((a >= 40 && a <= 50) && (b >= 40 && b <= 50)) {
+  return true;
+  }
+  return false
+}
+
+// Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+// Examples
+// max1020(11, 19) → 19
+// max1020(19, 11) → 19
+// max1020(11, 9) → 11
+
+function max1020(a, b){
+  const numA = (a >= 10 && a <= 20)
+  const numB = (b >= 10 && b <= 20)
+  if(numA && numB) {
+  return Math.max(a,b);
+  }else if(numA) {
+  return a;
+  }else if(numB) {
+  return b;
+  }
+    return 0;
+  }
+  
 // Given 2 strings, a and b, return the number of the positions where they contain
 // the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx"
 // "xx", "aa", and "az" substrings appear in the same place in both strings.
