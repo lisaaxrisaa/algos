9953707c668e94b2691a1df61e42337a4dbd475c
Lisa Fujita <lisafujita90@gmail.com>
2024-12-20 20:23:16 -0500

    JS string practice

--- string.js
+++ string.js
@@ -489,4 +489,20 @@ function sameEnds(str) {
   return false;
 }
 
-console.log(sameEnds('abcb'));
+// char 0, and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+
+// Examples
+
+// everyNth('Miracle', 2) → Mrce
+// everyNth('abcdefg', 2) → aceg
+// everyNth('abcdefg', 3) → adg
+
+function everyNth(str, n) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i += n) {
+    result += str[i];
+  }
+
+  return result;
+}
