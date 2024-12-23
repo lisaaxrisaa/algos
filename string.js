1b78f23eff3184ad2fdba276d79e548790c04a1e
Lisa Fujita <lisafujita90@gmail.com>
2024-12-23 14:23:58 -0500

    JS string practice

--- string.js
+++ string.js
@@ -530,3 +530,23 @@ function stringTimes(str, n) {
 function frontTimes(str, n) {
   return str.slice(0, 3).repeat(n);
 }
+
+// Count the number of 'xx' in the given string. We'll say that overlapping is allowed, so 'xxx' contains 2 'xx'.
+
+// Examples
+
+// countXX('abcxx') → 1
+// countXX('xxx') → 2
+// countXX('xxxx') → 3
+
+function countXX(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === 'x' && str[i + 1] === 'x') {
+      count++;
+    }
+  }
+
+  return count;
+}
