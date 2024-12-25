7955702f4e3639bb496bea01b03e9817f7cd515c
Lisa Fujita <lisafujita90@gmail.com>
2024-12-25 11:39:13 -0500

    JS string practice

--- string.js
+++ string.js
@@ -550,3 +550,20 @@ function countXX(str) {
 
   return count;
 }
+
+// Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+// Examples
+
+// doubleX('axxbb') → true
+// doubleX('axaxax') → false
+// doubleX('xxxxx') → true
+
+function doubleX(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] === 'x') {
+      return str[i + 1] === 'x';
+    }
+  }
+  return false;
+}
