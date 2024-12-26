5de61e3b2bf34c7220b350c9809cf0bdd274f907
Lisa Fujita <lisafujita90@gmail.com>
2024-12-26 15:59:55 -0500

    JS string practice

--- string.js
+++ string.js
@@ -567,3 +567,19 @@ function doubleX(str) {
   }
   return false;
 }
+
+// Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+// Examples
+
+// stringBits('Hello') → Hlo
+// stringBits('Hi') → H
+// stringBits('Heeololeo') → Hello
+
+function stringBits(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 2) {
+    result += str.charAt(i);
+  }
+  return result;
+}
