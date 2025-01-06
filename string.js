3c4a11510e2d319d9c5b8f2656e4db28e0522ac1
Lisa Fujita <lisafujita90@gmail.com>
2025-01-06 17:47:27 -0500

    JS string practice

--- string.js
+++ string.js
@@ -593,3 +593,24 @@ function stringSplosion(str) {
   }
   return result;
 }
+
+// Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+// Examples
+
+// last2('hixxhi') → 1
+// last2('xaxxaxaxx') → 1
+// last2('axxaaxx') → 1
+
+function last2(str) {
+  if (str.length < 2) return 0;
+  const last2 = str.slice(-2);
+  let count = 0;
+  for (let i = 0; i < str.length - 2; i++) {
+    if (str.slice(i, i + 2) === last2) {
+      count++;
+    }
+  }
+
+  return count;
+}
