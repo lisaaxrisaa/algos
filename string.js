1ea9f351903fc2aec5d7384eb44e571020420f6e
Lisa Fujita <lisafujita90@gmail.com>
2025-01-07 23:13:46 -0500

    JS string practice

--- string.js
+++ string.js
@@ -614,3 +614,21 @@ function last2(str) {
 
   return count;
 }
+
+// Given an array of ints, return the number of 9's in the array.
+
+// Examples
+
+// arrayCount9([1,2,9]) → 1
+// arrayCount9([1,9,9]) → 2
+// arrayCount9([1,9,9,3,9]) → 3
+
+function arrayCount9(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 9) {
+      count++;
+    }
+  }
+  return count;
+}
