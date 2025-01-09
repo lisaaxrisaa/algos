0c16b2f92f291c0b3267f29286a65988d7d8c200
Lisa Fujita <lisafujita90@gmail.com>
2025-01-08 22:41:26 -0500

    JS array practice

--- arrays.js
+++ arrays.js
@@ -445,3 +445,21 @@ class Solution {
     return false;
   }
 }
+
+// Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+// Examples
+
+// arrayFront9([1,2,9,3,4]) → true
+// arrayFront9([1,2,3,4,9]) → false
+// arrayFront9([1,2,3,4,5]) → false
+
+function arrayFront9(nums) {
+  const limit = Math.min(nums.length, 4); 
+  for (let i = 0; i < limit; i++) {
+    if (nums[i] === 9) {
+      return true; 
+    }
+  }
+  return false; 
+}
