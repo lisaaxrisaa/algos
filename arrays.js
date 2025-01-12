3232172928b7f0270bde10034346107167b3701c
Lisa Fujita <lisafujita90@gmail.com>
2025-01-12 16:24:23 -0500

    JS array practice

--- arrays.js
+++ arrays.js
@@ -463,3 +463,15 @@ function arrayFront9(nums) {
   }
   return false; 
 }
+
+// Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+function array123(nums){
+  for (let i = 0; i < nums.length - 2; i++) {
+  if (nums[i] === 1 && nums[i + 1] === 2 && nums[i + 2] === 3) {
+    return true; 
+  }
+}
+return false; 
+
+}
