a4570b9a4ddf0034693808d59f07b7dd1bc17d27
Lisa Fujita <lisafujita90@gmail.com>
2025-01-13 19:40:14 -0500

    JS string practice

--- string.js
+++ string.js
@@ -632,3 +632,24 @@ function arrayCount9(nums) {
   }
   return count;
 }
+
+// Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+
+// Examples
+
+// mirrorEnds('abXYZba') → ab
+// mirrorEnds('abca') → a
+// mirrorEnds('aba') → aba
+
+function mirrorEnds(string) {
+  let result = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[string.length - 1 - i]) {
+      result += string[i];
+    } else {
+      break;
+    }
+  }
+  return result;
+}
