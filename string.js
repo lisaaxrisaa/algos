d6b3bad73c2118f724de783d53d6750f31972ab3
Lisa Fujita <lisafujita90@gmail.com>
2025-01-20 10:53:33 -0500

    JS string practice

--- string.js
+++ string.js
@@ -678,3 +678,17 @@ function maxBlock(str) {
   }
   return Math.max(maxCount, currentCount);
 }
+
+// Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+
+// Examples
+
+// sumNumbers('abc123xyz') → 123
+// sumNumbers('aa11b33') → 44
+// sumNumbers('7 11') → 18
+
+function sumNumbers(str) {
+  let numbers = str.match(/\d+/g);
+  if (!numbers) return 0;
+  return numbers.reduce((sum, num) => sum + parseInt(num, 10), 0);
+}
