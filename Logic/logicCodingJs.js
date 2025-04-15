9b24eafacb58d032820214f91dea470cca95a8f5
Lisa Fujita <lisafujita90@gmail.com>
2025-04-15 17:23:51 -0400

    Implement noTeenSum with inner helper fixTeen to ignore most teen values

--- Logic/logicCodingJs.js
+++ Logic/logicCodingJs.js
@@ -703,3 +703,12 @@ if (a === 13) {
   return a + b; // c is 13 → a and b count, not c
 }
 return a + b + c; // no 13s → everything counts
+
+// ------------------------------------------------------
+
+// Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "public int fixTeen(int n) {"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main noTeenSum().
+
+// Examples
+// noTeenSum(1, 2, 3) → 6
+// noTeenSum(2, 13, 1) → 3
+// noTeenSum(2, 1, 14) → 3
