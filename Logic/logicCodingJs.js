0c6002a49cbb8678d5e7eff1d880bae6358c5d23
Lisa Fujita <lisafujita90@gmail.com>
2025-04-21 16:30:21 -0400

    Implement firstLast6 array check

--- Logic/logicCodingJs.js
+++ Logic/logicCodingJs.js
@@ -875,3 +875,16 @@ function makeChocolate(small, big, goal) {
     return -1;
   }
 }
+
+// ------------------------------------------------------
+
+// Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+
+// Examples
+// firstLast6([1, 2, 6]) → true
+// firstLast6([6, 1, 2, 3]) → true
+// firstLast6([13, 6, 1, 2, 3]) → false
+
+function firstLast6(nums) {
+  return nums[0] === 6 || nums[nums.length - 1] === 6;
+}
