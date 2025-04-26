303952c63a4539da88570282b730eca4e987e384
Lisa Fujita <lisafujita90@gmail.com>
2025-04-26 12:52:03 -0400

    Add sum3 function to calculate the sum of a 3-element array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -531,7 +531,20 @@ function commonEnd(a, b){
 
 // ---------------------------------------------------------
 
+// Given an array of ints length 3, return the sum of all the elements.
 
+// Examples
+// sum3([1, 2, 3]) → 6
+// sum3([5, 11, 2]) → 18
+// sum3([7, 0, 0]) → 7
+
+function sum3(nums){
+  let sum = 0
+  for(let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+  } 
+    return sum
+  }
 
 
 // ---------------------------------------------------------
