4ebed763cc8bd84e1cca6f645ac985cc66c33ce1
Lisa Fujita <lisafujita90@gmail.com>
2025-05-01 21:35:00 -0400

    Add maxEnd3 to return array of max(first, last) repeated 3 times

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -571,3 +571,17 @@ function rotateLeft3(nums){
 function reverse3(nums){
   return nums.reverse()
 }
+
+// ---------------------------------------------------------
+
+// Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+// Examples
+// maxEnd3([1, 2, 3]) → 3,3,3
+// maxEnd3([11, 5, 9]) → 11,11,11
+// maxEnd3([2, 11, 3]) → 3,3,3
+
+function maxEnd3(nums){
+  let max = Math.max(nums[0],nums[1],nums[2])
+  return Array(3).fill(max)
+}
