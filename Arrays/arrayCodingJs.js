d072bb2b7ce8793d5f3fb5c0747fb9203a5876d9
Lisa Fujita <lisafujita90@gmail.com>
2025-05-03 12:04:22 -0400

    Add middleWay to return middle elements from two 3-element arrays

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -600,3 +600,17 @@ function sum2(nums){
     if(nums.length === 1) return nums[0]
    return nums[0]+nums[1]
   }
+
+  // ---------------------------------------------------------
+
+//   Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+// Examples
+// middleWay([1, 2, 3], [4, 5, 6]) → 2,5
+// middleWay([7, 7, 7], [3, 8, 0]) → 7,8
+// middleWay([5, 2, 9], [1, 4, 5]) → 2,4
+
+function middleWay(a, b){
+  return [a[1], b[1]]
+}
+
