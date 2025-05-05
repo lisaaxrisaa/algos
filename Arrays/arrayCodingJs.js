5d2c37a596c9345e07f152a944d56fb7ac8106df
Lisa Fujita <lisafujita90@gmail.com>
2025-05-05 13:51:22 -0400

    Add has23 to check if array includes 2 or 3

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -626,3 +626,17 @@ function middleWay(a, b){
 function makeEnds(nums){
   return [nums[0],nums[nums.length -1]]
 }
+
+  // ---------------------------------------------------------
+
+  // Given an int array length 2, return true if it contains a 2 or a 3.
+
+  // Examples
+  // has23([2, 5]) → true
+  // has23([4, 3]) → true
+  // has23([4, 5]) → false 
+
+  function has23(nums){
+    if(nums.includes(2) || nums.includes(3)) return true
+    return false
+  }
