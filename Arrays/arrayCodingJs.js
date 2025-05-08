c7d8f2d21d8818ddebdc665b96b1a18dd1639bb1
Lisa Fujita <lisafujita90@gmail.com>
2025-05-07 21:21:41 -0400

    Add no23 to check that array does not contain 2 or 3

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -640,3 +640,17 @@ function makeEnds(nums){
     if(nums.includes(2) || nums.includes(3)) return true
     return false
   }
+
+    // ---------------------------------------------------------
+
+//     Given an int array length 2, return true if it does not contain a 2 or 3.
+
+// Examples
+// no23([4, 5]) → true
+// no23([4, 2]) → false
+// no23([3, 5]) → false
+
+function no23(nums){
+  if(nums.includes(2) || nums.includes(3)) return false
+  return true
+}
