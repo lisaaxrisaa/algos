95a75b1bae89f3cab05e386f367a033860ed7898
Lisa Fujita <lisafujita90@gmail.com>
2025-05-08 10:24:00 -0400

    Add makeLast to return double-length array ending in original last element

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -654,3 +654,18 @@ function no23(nums){
   if(nums.includes(2) || nums.includes(3)) return false
   return true
 }
+
+    // ---------------------------------------------------------
+
+//     Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more.
+
+// Examples
+// makeLast([4, 5, 6]) → 0,0,0,0,0,6
+// makeLast([1, 2]) → 0,0,0,2
+// makeLast([3]) → 0,3
+
+function makeLast(nums){
+  let result = new Array(nums.length * 2).fill(0);
+result[result.length - 1] = nums[nums.length - 1];
+return result;
+}
