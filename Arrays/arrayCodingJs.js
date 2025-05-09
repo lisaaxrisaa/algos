d2d2068b766b420529dce6f9880a333eda270bdc
Lisa Fujita <lisafujita90@gmail.com>
2025-05-09 09:40:46 -0400

    Add double23 to return true if array contains exactly two 2s or two 3s

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -669,3 +669,24 @@ function makeLast(nums){
 result[result.length - 1] = nums[nums.length - 1];
 return result;
 }
+
+    // ---------------------------------------------------------
+
+// Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+
+// Examples
+// double23([2, 2]) → true
+// double23([3, 3]) → true
+// double23([2, 3]) → false
+
+function double23(nums){
+  let count2 = 0;
+  let count3 = 0;
+
+  for (let num of nums) {
+    if (num === 2) count2++;
+    if (num === 3) count3++;
+  }
+
+  return count2 === 2 || count3 === 2;
+}
