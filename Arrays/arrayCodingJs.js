27d1ac0150ae9afad3d2421bb36472684e40d787
Lisa Fujita <lisafujita90@gmail.com>
2025-05-12 22:54:01 -0400

    Add start1 function to count how many arrays start with 1

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -709,3 +709,19 @@ function fix23(nums) {
   }
   return nums;
 }
+
+ // ---------------------------------------------------------
+
+//  Start with 2 int arrays, a and b, of any length. Return how many of the arrays have 1 as their first element.
+
+// Examples
+// start1([1, 2, 3], [1, 3]) → 2
+// start1([7, 2, 3], [1]) → 1
+// start1([1, 2], []) → 1
+
+function start1(a, b){
+  let count = 0
+  if(a[0] === 1) count ++
+  if(b[0] === 1) count ++
+  return count
+}
