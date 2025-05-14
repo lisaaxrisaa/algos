1e6206e4ea035a6d7b2e61731266b04b59a05532
Lisa Fujita <lisafujita90@gmail.com>
2025-05-13 23:35:07 -0400

    Add biggerTwo to return array with larger sum, default to a on tie

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -725,3 +725,20 @@ function start1(a, b){
   if(b[0] === 1) count ++
   return count
 }
+
+ // ---------------------------------------------------------
+
+//  Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+
+// Examples
+// biggerTwo([1, 2], [3, 4]) → 3,4
+// biggerTwo([3, 4], [1, 2]) → 3,4
+// biggerTwo([1, 1], [1, 2]) → 1,2
+
+function biggerTwo(a, b){
+let sumA = a[0] + a[1];
+let sumB = b[0] + b[1];
+if (sumA >= sumB) return a;
+return b;
+
+}
