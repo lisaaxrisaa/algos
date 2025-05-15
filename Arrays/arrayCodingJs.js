1b2ca2b81ac3e7a26da600753540a1de4a8d37ec
Lisa Fujita <lisafujita90@gmail.com>
2025-05-15 19:18:22 -0400

    Add plusTwo to combine two 2-element arrays into one

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -756,3 +756,16 @@ function makeMiddle(nums){
   let mid = nums.length / 2;
 return [nums[mid - 1], nums[mid]];
 }
+
+ // ---------------------------------------------------------
+
+// Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+
+// Examples
+// plusTwo([1, 2], [3, 4]) → 1,2,3,4
+// plusTwo([4, 4], [2, 2]) → 4,4,2,2
+// plusTwo([9, 2], [3, 4]) → 9,2,3,4
+
+function plusTwo(a, b){
+  return [a[0],a[1],b[0],b[1]]
+}
