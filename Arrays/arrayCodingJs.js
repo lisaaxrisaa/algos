52d865cbfdf42cd4b3036a0818d42b9cc67b50ff
Lisa Fujita <lisafujita90@gmail.com>
2025-05-14 21:58:29 -0400

    Add makeMiddle to return middle two elements of even-length array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -742,3 +742,17 @@ if (sumA >= sumB) return a;
 return b;
 
 }
+
+ // ---------------------------------------------------------
+
+//  Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+
+// Examples
+// makeMiddle([1, 2, 3, 4]) → 2,3
+// makeMiddle([7, 1, 2, 3, 4, 9]) → 2,3
+// makeMiddle([1, 2]) → 1,2
+
+function makeMiddle(nums){
+  let mid = nums.length / 2;
+return [nums[mid - 1], nums[mid]];
+}
