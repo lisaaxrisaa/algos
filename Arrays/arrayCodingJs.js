47a48d3a1f8b3d46bbcab914649c341afbb66828
Lisa Fujita <lisafujita90@gmail.com>
2025-05-16 23:30:35 -0400

    Add swapEnds to swap first and last elements of an array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -769,3 +769,21 @@ return [nums[mid - 1], nums[mid]];
 function plusTwo(a, b){
   return [a[0],a[1],b[0],b[1]]
 }
+
+ // ---------------------------------------------------------
+
+//  Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
+
+// Examples
+// swapEnds([1, 2, 3, 4]) → 4,2,3,1
+// swapEnds([1, 2, 3]) → 3,2,1
+// swapEnds([8, 6, 7, 9, 5]) → 5,6,7,9,8
+
+function swapEnds(nums){
+  if (nums.length >= 2) {
+  let temp = nums[0];
+  nums[0] = nums[nums.length - 1];
+  nums[nums.length - 1] = temp;
+}
+return nums;
+}
