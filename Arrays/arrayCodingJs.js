ffac7ebfe046fd3764b2f75424805da7a67e7281
Lisa Fujita <lisafujita90@gmail.com>
2025-05-25 23:45:14 -0400

    Add front11 to return first elements of a and b if they exist

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -862,3 +862,23 @@ function make2(a, b){
 
  return combined.slice(0, 2);
 }
+
+ // ---------------------------------------------------------
+
+//  Given 2 int arrays, a and b, of any length, return a new array with the first element of each array. If either array is length 0, ignore that array.
+
+// Examples
+// front11([1, 2, 3], [7, 9, 8]) → 1,7
+// front11([1], [2]) → 1,2
+// front11([1, 7], []) → 1
+
+function front11(a, b) {
+  const result = [];
+  if (a.length > 0) {
+    result.push(a[0]);
+  }
+  if (b.length > 0) {
+    result.push(b[0]);
+  }
+  return result;
+}
