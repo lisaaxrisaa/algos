ff2b33e4577cb71e463aab7c9e78341860d56f36
Lisa Fujita <lisafujita90@gmail.com>
2025-05-26 22:28:03 -0400

    Add findLowestIndex to return index of smallest value in array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -882,3 +882,24 @@ function front11(a, b) {
   }
   return result;
 }
+
+ // ---------------------------------------------------------
+
+//  Return the index of the minimum value in an array. The input array will have at least one element in it.
+
+// Examples
+// findLowestIndex([99, 98, 97, 96, 95]) → 4
+// findLowestIndex([2, 2, 0]) → 2
+// findLowestIndex([1, 3, 5]) → 0
+
+function findLowestIndex(nums){
+  let minIndex = 0;
+
+ for (let i = 1; i < nums.length; i++) {
+   if (nums[i] < nums[minIndex]) {
+     minIndex = i;
+   }
+ }
+
+ return minIndex;
+}
