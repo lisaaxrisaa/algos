c9f4c4b9c5eff91fd655a5ba794fae705db3d2cb
Lisa Fujita <lisafujita90@gmail.com>
2025-05-29 21:14:44 -0400

    feat: add bigDiff function to calculate difference between max and min in array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -924,3 +924,18 @@ function countEvens(nums) {
 
   return count;
 }
+
+ // ---------------------------------------------------------
+
+//  Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
+
+// Examples
+// bigDiff([10, 3, 5, 6]) → 7
+// bigDiff([7, 2, 10, 9]) → 8
+// bigDiff([2, 10, 7, 2]) → 8
+
+function bigDiff(nums){
+  const max = Math.max(...nums); 
+ const min = Math.min(...nums); 
+ return max - min;  
+}
