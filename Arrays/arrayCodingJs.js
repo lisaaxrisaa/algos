ee3be9b2c9fab056813bd95f8024da05a38967ba
Lisa Fujita <lisafujita90@gmail.com>
2025-05-30 15:04:45 -0400

    feat: add centeredAverage function to calculate trimmed mean of array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -939,3 +939,20 @@ function bigDiff(nums){
  const min = Math.min(...nums); 
  return max - min;  
 }
+
+ // ---------------------------------------------------------
+
+//  Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+
+// Examples
+// centeredAverage([1, 2, 3, 4, 100]) → 3
+// centeredAverage([1, 1, 5, 5, 10, 8, 7]) → 5.2
+// centeredAverage([-10, -4, -2, -4, -2, 0]) → -3
+
+function centeredAverage(nums){
+let sorted = [...nums].sort((a, b) => a - b);
+sorted.pop();    
+sorted.shift();   
+let sum = sorted.reduce((acc, num) => acc + num, 0);
+return sum / sorted.length;
+}
