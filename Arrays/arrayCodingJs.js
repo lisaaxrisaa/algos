d3771bf2a7e79fe6efc6aa588cfa0819fb7aab34
Lisa Fujita <lisafujita90@gmail.com>
2025-06-02 11:53:26 -0400

    feat: implement sum13 to exclude 13 and the number after from sum

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -956,3 +956,24 @@ sorted.shift();
 let sum = sorted.reduce((acc, num) => acc + num, 0);
 return sum / sorted.length;
 }
+
+ // ---------------------------------------------------------
+
+//  Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+
+// Examples
+// sum13([1, 2, 2, 1]) → 6
+// sum13([1, 1]) → 2
+// sum13([1, 2, 2, 1, 13]) → 6
+
+function sum13(nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 13) {
+      i++; 
+      continue;
+    }
+    sum += nums[i];
+  }
+  return sum;
+}
