cc80fb0d1f7f0a574cacc85312e7403cbfd72188
Lisa Fujita <lisafujita90@gmail.com>
2025-06-05 12:55:30 -0400

    feat: add sum67 to skip summing values between 6 and next 7

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -977,3 +977,29 @@ function sum13(nums) {
   }
   return sum;
 }
+
+ // ---------------------------------------------------------
+
+//  Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+// Examples
+// sum67([1, 2, 2]) → 5
+// sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+// sum67([1, 1, 6, 7, 2]) → 4
+
+function sum67(nums){
+  let sum = 0;
+let inBlock = false;
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 6) {
+    inBlock = true;
+  } else if (inBlock && nums[i] === 7) {
+    inBlock = false;
+  } else if (!inBlock) {
+    sum += nums[i];
+  }
+}
+
+return sum;
+}
