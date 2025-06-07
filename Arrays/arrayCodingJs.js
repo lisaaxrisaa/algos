c1df41fbf34094a7cf756cc655ab0e78f6d835b0
Lisa Fujita <lisafujita90@gmail.com>
2025-06-07 11:37:14 -0400

    feat: implement lucky13 to check for absence of 1s and 3s in array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1021,3 +1021,20 @@ function has22(nums) {
   }
   return false;
 }
+
+// ---------------------------------------------------------
+// Given an array of ints, return true if the array contains no 1's and no 3's.
+
+// Examples
+// lucky13([0, 2, 4]) → true
+// lucky13([1, 2, 3]) → false
+// lucky13([1, 2, 4]) → false
+
+function lucky13(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1 || nums[i] === 3) {
+      return false;
+    }
+  }
+  return true;
+}
