9ec1a88cf354768695fcbf7f1c19f4bd3b634229
Lisa Fujita <lisafujita90@gmail.com>
2025-06-06 23:25:30 -0400

    feat: implement has22 to check for adjacent 2s in array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1003,3 +1003,21 @@ for (let i = 0; i < nums.length; i++) {
 
 return sum;
 }
+
+// ---------------------------------------------------------
+
+// Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+
+// Examples
+// has22([1, 2, 2]) → true
+// has22([1, 2, 1, 2]) → false
+// has22([2, 1, 2]) → false
+
+function has22(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === 2 && nums[i + 1] === 2) {
+      return true;
+    }
+  }
+  return false;
+}
