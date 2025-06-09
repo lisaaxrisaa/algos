e8f658d15634d2cbd814e3eeffab71b17152b056
Lisa Fujita <lisafujita90@gmail.com>
2025-06-08 21:15:59 -0400

    feat: implement sum28 to return true if total sum of 2s equals 8

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1038,3 +1038,21 @@ function lucky13(nums) {
   }
   return true;
 }
+
+// ---------------------------------------------------------
+// Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+
+// Examples
+// sum28([2, 3, 2, 2, 4, 2]) → true
+// sum28([2, 3, 2, 2, 4, 2, 2]) → false
+// sum28([1, 2, 3, 4]) → false
+
+function sum28(nums){
+  let sum = 0
+  for(let i = 0; i < nums.length; i++) {
+  if(nums[i] === 2) {
+  sum += nums[i]
+  }
+  }
+  return sum === 8
+}
