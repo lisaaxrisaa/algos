5558b1281e1d64a3fd01b72219eea3c6549dfa50
Lisa Fujita <lisafujita90@gmail.com>
2025-06-10 22:34:44 -0400

    Add prependSum function to combine and replace first two elements

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1077,3 +1077,18 @@ function more14(nums) {
 
   return ones > fours;
 }
+
+// ---------------------------------------------------------
+
+// Return a modified version of the input array (nums), where the first two items have been removed and one item – the sum of those two items - is added to the start of the array.
+
+// Examples
+// prependSum([1, 2, 4, 4]) → 3,4,4
+// prependSum([3, 3, 0]) → 6,0
+// prependSum([1, 1, 1, 1, 1]) → 2,1,1,1
+
+function prependSum(nums){
+  const sum = nums[0] + nums[1];
+const rest = nums.slice(2);
+return [sum, ...rest];
+}
