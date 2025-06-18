14e2006528ca9c52e3027d63a9b38b054e31bf8b
Lisa Fujita <lisafujita90@gmail.com>
2025-06-17 22:56:48 -0400

    Implement only14 function to check if all array elements are 1 or 4

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1109,3 +1109,21 @@ for (let i = 0; i < n; i++) {
 }
 return result;
 }
+
+// ---------------------------------------------------------
+
+// Given an array of ints, return true if every element is a 1 or a 4.
+
+// Examples
+// only14([1, 4, 1, 4]) → true
+// only14([1, 4, 2, 4]) → false
+// only14([1, 1]) → true
+
+function only14(nums){
+  for (let i = 0; i < nums.length; i++) {
+  if (nums[i] !== 1 && nums[i] !== 4) {
+    return false;
+  }
+}
+return true;
+}
