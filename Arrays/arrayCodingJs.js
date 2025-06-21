4d289a6129f96b63dc08ea5240d31b93e12fe7dd
Lisa Fujita <lisafujita90@gmail.com>
2025-06-21 15:25:06 -0400

    Add no14 function to check for exclusive presence of 1s or 4s in array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1142,3 +1142,22 @@ function fizzArray2(n) {
   }
   return result;
 }
+
+// Given an array of ints, return true if it contains no 1's or it contains no 4's.
+
+// Examples
+// no14([1, 2, 3]) → true
+// no14([1, 2, 3, 4]) → false
+// no14([2, 3, 4]) → true
+
+function no14(nums) {
+  let has1 = false;
+  let has4 = false;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) has1 = true;
+    if (nums[i] === 4) has4 = true;
+  }
+
+  return !(has1 && has4); 
+}
