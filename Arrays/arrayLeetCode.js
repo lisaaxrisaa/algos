13b2b8778d27bf1085ca20ccbe72f71437b2a69c
Lisa Fujita <lisafujita90@gmail.com>
2025-06-23 19:50:00 -0400

    560 Subarray Sum Equals K

--- Arrays/arrayLeetCode.js
+++ Arrays/arrayLeetCode.js
@@ -822,3 +822,32 @@ var minSubArrayLen = function (target, nums) {
   }
   return minLength === Infinity ? 0 : minLength;
 };
+
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+var subarraySum = function (nums, k) {
+  let count = 0;
+  let sum = 0;
+  let map = new Map();
+  map.set(0, 1);
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+  return count;
+};
