ace5cada3d37e1158239bdec5c77e21653a0888e
Lisa Fujita <lisafujita90@gmail.com>
2025-07-04 07:23:26 -0400

    Implement either24 logic to detect adjacent 2s or 4s but not both

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1177,3 +1177,26 @@ function isEverywhere(nums, val){
 }
 return true;
 }
+
+// Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+
+// Examples
+// either24([1, 2, 2]) → true
+// either24([4, 4, 1]) → true
+// either24([4, 4, 1, 2, 2]) → false
+
+function either24(nums){
+  let has22 = false;
+let has44 = false;
+
+for (let i = 0; i < nums.length - 1; i++) {
+  if (nums[i] === 2 && nums[i + 1] === 2) {
+    has22 = true;
+  }
+  if (nums[i] === 4 && nums[i + 1] === 4) {
+    has44 = true;
+  }
+}
+
+return has22 !== has44; 
+}
