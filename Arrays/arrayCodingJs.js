3e658668657feb66d8496893a1732350f629fee8
Lisa Fujita <lisafujita90@gmail.com>
2025-07-14 20:44:55 -0400

    Add twoTwo function to ensure all 2s in array appear in pairs

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1280,3 +1280,21 @@ for (let i = 0; i < nums.length; i++) {
 
 return count === 3;
 }
+
+// Given an array of ints, return true if every 2 that appears in the array is next to another 2.
+
+// Examples
+// twoTwo([4, 2, 2, 3]) → true
+// twoTwo([2, 2, 4]) → true
+// twoTwo([2, 2, 4, 2]) → false
+
+function twoTwo(nums){
+  for (let i = 0; i < nums.length; i++) {
+  if (nums[i] === 2) {
+    if (nums[i - 1] !== 2 && nums[i + 1] !== 2) {
+      return false;
+    }
+  }
+}
+return true;
+}
