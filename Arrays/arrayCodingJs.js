d2947ee400cffe25718ce9b2da32d20998b718f4
Lisa Fujita <lisafujita90@gmail.com>
2025-07-16 23:05:51 -0400

    Add tripleUp function to check for 3 increasing adjacent numbers

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1298,3 +1298,22 @@ function twoTwo(nums){
 }
 return true;
 }
+
+// Return true if the array contains, somewhere, three increasing adjacent numbers like .... 4, 5, 6, ... or 23, 24, 25.
+
+// Examples
+// tripleUp([1, 4, 5, 6, 2]) → true
+// tripleUp([1, 2, 3]) → true
+// tripleUp([1, 2, 4]) → false
+
+function tripleUp(nums){
+  for (let i = 0; i < nums.length - 2; i++) {
+  if (
+    nums[i] + 1 === nums[i + 1] &&
+    nums[i + 1] + 1 === nums[i + 2]
+  ) {
+    return true;
+  }
+}
+return false;
+}
