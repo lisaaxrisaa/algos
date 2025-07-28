8ae0d12b5386562184cc66d28605532eb91b39a7
Lisa Fujita <lisafujita90@gmail.com>
2025-07-27 20:13:52 -0400

    Implement pre4 to return all elements before the first 4 in the array

--- Arrays/arrayCodingJs.js
+++ Arrays/arrayCodingJs.js
@@ -1374,3 +1374,15 @@ function tenRun(nums) {
 
   return result;
 }
+
+// Given a non-empty array of ints, return a new array containing the elements from the original array that come before the first 4 in the original array. The original array will contain at least one 4. Note that it is valid in java to create an array of length 0.
+
+// Examples
+// pre4([1, 2, 4, 1]) → 1,2
+// pre4([3, 1, 4]) → 3,1
+// pre4([1, 4, 4]) → 1
+
+function pre4(nums){
+  let index = nums.indexOf(4); 
+ return nums.slice(0, index);
+}
